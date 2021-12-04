5b0de5c0e30ab2b46ed5c7aad69a4aeb3b85b957
Umratul <umratul0904@gmail.com>
2021-12-05 01:34:04 +0600

    first

---
+++ data/data.js
@@ -0,0 +1,22 @@
+const details = [
+
+    {
+        id: 1,
+        name: 'Travelo',
+        img: 'https://ibb.co/dcZ5NtN',
+        body: 'lorem5'  
+    },
+    {
+        id: 2,
+        name: 'CarRev',
+        img: 'https://ibb.co/qnh9Yj2',
+        body: 'lorem5'  
+    },
+    {
+        id: 3,
+        name: 'Dental Monte',
+        img: 'https://ibb.co/nsTq6vW',
+        body: 'lorem5'  
+    }
+
+]
