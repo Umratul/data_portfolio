c4dfab3b49e8ac27ccb9f9cdc64187986b267210
Umratul <umratul0904@gmail.com>
2021-12-16 01:21:04 +0600

    details add1

--- data/data.js
+++ data/data.js
@@ -4,7 +4,8 @@ const details = [
         id: 1,
         name: 'Travelo',
         img: 'https://i.ibb.co/j5trCzC/project1.png',
-        body: 'lorem5'  
+        body: ' - 	A full Stack Responsive Website .- Made with React,Firebase,React Router,React Bootstrap etc.- Data is loaded by MongoDb .'            
+         
     },
     {
         id: 2,
